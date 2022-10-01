abc9cfa30da804860a797cb5cd251821ad165dcb
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-01 20:28:18 +0300

    1 5

---
+++ 1 zadacha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число 1");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int numberB = int.Parse(Console.ReadLine());
+ if (numberA==numberB * numberB)
+ {
+       Console.WriteLine("yes"); 
+ }
+else
+{
+    Console.WriteLine("no");
+} 
---
+++ zadacha 5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число ");
+int numberA = int.Parse(Console.ReadLine());
+int i =-numberA;
+
+while (i<=numberA)
+{
+    Console.Write (i +",");
+    i++;
+}

e3f2aea3c460dc0043e3821ed511f77d326be78d
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-01 20:42:48 +0300

    3

---
+++ 2 zadacha/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число от 1-7 ");
+int numberA = int.Parse(Console.ReadLine());
+if(numberA==1)
+{
+    Console.WriteLine("понедельник"); 
+}
+if(numberA==2)
+{
+    Console.WriteLine("вторник"); 
+}
+if(numberA==3)
+{
+        Console.WriteLine("среда"); 
+}
+if(numberA==4)
+{
+        Console.WriteLine("четверг"); 
+}
+if(numberA==5)
+{
+        Console.WriteLine("пятница"); 
+}
+if(numberA==6)
+{
+        Console.WriteLine("суббота"); 
+}
+if(numberA==7)
+{
+ Console.WriteLine("воскресенье"); 
+}
